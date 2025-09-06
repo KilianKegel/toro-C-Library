b2ac58e86e9b658985a56de55dbb238bd2a735ca
KilianKegel <kilian_kegel@hotmail.com>
2025-09-06 18:44:35 +0200

    ### 20250906, v0.9.6 Build 757 * fix buffer flush in `fprintf()`-family functions

--- buildLLVM.bat
+++ buildLLVM.bat
@@ -4,7 +4,7 @@ echo Compiling the C source with the LLVM tool chain...
 CLANG-cl /nologo /c /GS- /D_NO_CRT_STDIO_INLINE /D_CRT_SECURE_NO_WARNINGS WELCOME.c
 
 echo Linking the .OBJ to UEFI SHELL Executable WELCOME.EFI 
-LLD-link /nologo /NODEFAULTLIB /ENTRY:_cdeCRT0UefiShell /OUT:welcome.efi /SUBSYSTEM:EFI_APPLICATION WELCOME.obj toroC64LLVMUefiShell.lib
+LLD-link /nologo /MAP:welcomeLLVM.efi.map /NODEFAULTLIB /ENTRY:_cdeCRT0UefiShell /OUT:welcome.efi /SUBSYSTEM:EFI_APPLICATION WELCOME.obj toroC64LLVMUefiShell.lib
 
 echo Linking the .OBJ to Windows NT Executable WELCOME.EXE
-LLD-link /nologo /NODEFAULTLIB /ENTRY:_cdeCRT0WinNT /OUT:welcome.exe /SUBSYSTEM:CONSOLE WELCOME.obj toroC64LLVMWinNT.lib KERNEL32.LIB
+LLD-link /nologo /MAP:welcomeLLVM.exe.map /NODEFAULTLIB /ENTRY:_cdeCRT0WinNT /OUT:welcome.exe /SUBSYSTEM:CONSOLE WELCOME.obj toroC64LLVMWinNT.lib KERNEL32.LIB
